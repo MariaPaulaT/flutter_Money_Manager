9f0f66362e178e3c1d82203badb87b215a4b85fd
MariaPaulaT <maria.tellez.vallejo@accenture.com>
2023-03-06 14:56:42 -0500

    Transaction class

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,11 +1,48 @@
-
 import 'package:flutter/material.dart';
+import './transaction.dart';
+void main() {
+  //takes a widget and runs it
+  runApp(MyApp());
+}
 
-class name extends StatelessWidget {
-  
+
+class MyApp extends StatelessWidget {
+
+final List<Transaction> transactions =[
+  Transaction(
+    id: 't1', 
+    title: 'New Shoes', 
+    amount: 69.99, 
+    date: DateTime.now(),)
+];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Money manager')
+      ),
+      body:Column(
+        //SpaceAround: the amount of space betweeen the items is the same
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //Position items, cross axis aligment, from the right to the left
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        //If we want to change the size of a card, we should change the size of the child
+        Container(
+          width: double.infinity,
+          child: Card(
+            child: Text('Chart'),
+            color: Colors.blue,
+            elevation: 5,
+            ),
+        ),
+        Card(
+          color: Colors.red,
+          child: Text('LL'),
+          )
+      ],),
+    );
+
   }
 }
---
+++ first_app/lib/transaction.dart
@@ -0,0 +1,16 @@
+//Its not a widget, its an object that we will like to use in our code
+
+class Transaction{
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    this.id, 
+    this.title, 
+    this.amount,
+    this.date,
+    });
+
+}

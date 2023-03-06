732258d3cf324a855dd2d133ca67ba231d1efb59
MariaPaulaT <maria.tellez.vallejo@accenture.com>
2023-03-06 12:50:34 -0500

    Beggining with the app

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,58 +1,11 @@
-// 1) Create a new Flutter App (in this project) and output an AppBar and some text
-// below it
-// 2) Add a button which changes the text (to any other text of your choice)
-// 3) Split the app into three widgets: App, TextControl & Text
 
 import 'package:flutter/material.dart';
-import './text.dart';
-import './textControl.dart';
-void main() {
-  //takes a widget and runs it
-  runApp(MyApp());
-}
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
+class name extends StatelessWidget {
   
-    return MyAppState();
-  }
-}
-
-class MyAppState extends State<MyApp>{
-
-  var text = 'Soy Maria';
 
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp(
-      home: Scaffold(
-      appBar: AppBar(
-        title: Text('Challenge'),
-        ),
-        body: Column(children: [
-          Text(text),
-         TextControl(selectHandler, text),
-
-        ],)
-      )
-      );
-
-  }
-
-
-
-  String selectHandler() {
-
-   setState(() {
-        if(text == 'Soy Juan'){
-          text = 'Soy Maria';
-        }else{
-           text = 'Soy Juan';
-        }
-   });
-
-   return text;
+    return Container();
   }
 }
